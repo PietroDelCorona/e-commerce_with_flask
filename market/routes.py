23fe453bc3c2d0a9866e21a63701038ab9c7911c
Pietro Del Corona <pietrogdelcorona@gmail.com>
2024-07-29 18:31:12 -0300

    installed flask-form, created register form model and page

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from market import app
 from market.models import Item
+from market.forms import RegisterForm
 
 @app.route("/")
 @app.route("/home")
@@ -8,9 +9,14 @@ def home_page():
     return render_template("home.html")
 
 @app.route("/market")
-def market():
+def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
+
+@app.route("/register")
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 
 if __name__ == "__main__":
