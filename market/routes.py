5903d99f36eaa9acfe3a99de66d8b47b7fba24ec
Pietro Del Corona <pietrogdelcorona@gmail.com>
2024-07-30 11:46:14 -0300

    flashes advanced_validation, response errors changes base

--- market/routes.py
+++ market/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market import app
 from market.models import Item, User
 from market.forms import RegisterForm
@@ -26,7 +26,8 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+    
     return render_template('register.html', form=form)
 
 
