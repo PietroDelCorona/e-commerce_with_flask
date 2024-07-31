e48e18b01c1266d87c309005b27818f5fcfc4238
Pietro Del Corona <pietrogdelcorona@gmail.com>
2024-07-31 16:31:02 -0300

    logout, customizations login

--- market/__init__.py
+++ market/__init__.py
@@ -8,7 +8,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '9d2bfccb58d1b5c18f4bcc73'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
 
