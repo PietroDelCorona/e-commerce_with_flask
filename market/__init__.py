3cc8550b1a3a129af53e71297aadfb271c5d1e80
Pietro Del Corona <pietrogdelcorona@gmail.com>
2024-08-02 11:56:10 -0300

    Add readme and security issues

--- market/__init__.py
+++ market/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 
 db = SQLAlchemy(app)
